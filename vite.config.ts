1d777eafc78ad79439c79e809043c77383751986
Aizhan Tilek <aijanm01@gmail.com>
2025-09-06 11:14:13 +0600

    Fix: vite config

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig(({ mode }) => {
 
   return {
     plugins: [react()],
+    base: process.env.VITE_BASE_PATH || "/explore",
     build: {
       commonjsOptions: { transformMixedEsModules: true },
     },
